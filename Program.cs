275864497222c44caa430a971e3785c15e168640
ARIADNA <rariadna@inbox.ru>
2022-10-16 17:39:11 +0300

    Task 2 correction of mistakes

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает 2 числа и выдает,
 // какое число большее, а какое меньшее. 
 Console.Write("Введите 1-е число ");
-int a = int.Parse(Console.ReadLine ()!);
+int num1 = int.Parse(Console.ReadLine()!);
 Console.Write("Введите 2-е число ");
-int b = int.Parse(Console.ReadLine ()!);
-if (a>b)
+int num2 = int.Parse(Console.ReadLine()!);
+if (num1 > num2)
 {
-    Console.Write($"{int a} - максимальное число");
+    Console.Write($"{num1} - максимальное число");
 }
-else (b>a)
+else (num2 > num1)
 {
-    Console.Write($"{int b} - максимальное число");
+    Console.Write($"{num2} - максимальное число");
 }
